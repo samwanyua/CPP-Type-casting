aefbcbac0068a30213dbeb33de3d800f832b3268
samwanyua <samexample8@gmail.com>
2024-03-12 17:16:34 +0300

    Intro to type casting in c++

---
+++ Intro.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   cout << "Hello Sam" << endl;
+   /*
+   * Type casting/type conversion - conversion from one type to another
+   * There's implicit and explicit type conversions
+   *  Implicit/automatic type casting: for example. char a + int b;
+   *        - done by compiler on its own
+   *        - Expressions contains one or more  data type
+            * Type promotion:
+            *    bool -> char -> short int -> int -> unsigned int -> long ->
+            *    unsigned long -> long long -> float -> double -> long double
+            * Can result to loss of infomation(sign, overflow)
+   * 
+   * 
+   * 
+   * Explicit type casting
+   *        - C-sytle typecasting es. (int a)
+   *        - Using casting operator:
+   *                >> static_cast<>
+   *                >> dynamic_cast<>
+   *                >> const_cast<>
+   *                >> reinterpret_cast<>
+   *                
+   * 
+    */
+    return 0;
+}
